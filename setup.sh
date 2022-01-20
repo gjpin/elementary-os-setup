385ab3697ac8a0d6937cbbf59f324bd973c79b6d
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-01-20 19:31:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,11 +53,11 @@ sudo systemctl enable --now syncthing@${USER}.service
 # Install Hashi stack
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository --yes "deb [arch=amd64] https://apt.releases.hashicorp.com focal main"
-sudo apt update && sudo apt install consul nomad terraform vault
+sudo apt update && sudo apt -y install consul nomad terraform vault
 
 # Install Ansible
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt -y install ansible
 
 # Install Golang
 wget https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz
